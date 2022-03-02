fbaf3b75d9d2769ec356c76d4168a0962bc9bdfe
Daniel <danilog1000@gmail.com>
2022-03-02 09:49:40 -0500

    Backend first commit

---
+++ routes/todoRoutes.js
@@ -0,0 +1,81 @@
+const router = require("express").Router();
+const Todo = require("../models/todo");
+const Record = require("../models/record");
+const { ObjectId } = require('mongodb');
+
+router.get("/search_all", async (req, res) => {
+  try {
+    const resul = await Todo.find();
+    res.json(resul);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/record", async (req, res) => {
+  try {
+    const resul = await Record.find();
+    res.json(resul);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post("/create", async (req, res) => {
+  try {
+    const resul = await Todo.create(req.body);
+    res.send(req.body);
+  }catch (err){
+    res.status(500).send(err);
+  }
+});
+
+router.put("/update:id", async (req, res) => {
+  //const resul = await Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+  const id = ObjectId(req.params.id);
+  const resul = await Todo.findOneAndUpdate({ _id: id}, req.body, {new: true});
+  try {
+    res.send(resul);
+  }catch (err){
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
+
+
+
+/**
+const router = require("express").Router();
+const Todo = require("../models/Todo");
+
+router.get("/", (req, res) => {
+    Todo.find((err, result) => {
+        if(err) throw new Error(err);
+        res.json(result);
+    });
+});
+
+router.post("/", (req, res) => {
+    Todo.create(req.body, (err, result) => {
+        if(err) throw new Error(err);
+        res.json(result);
+    });
+});
+
+router.put("/:id", (req, res) => {
+    Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
+        if(err) throw new Error(err);
+        res.json(result);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    Todo.findOneAndRemove({ _id: req.params.id }, (err, result) => {
+        if(err) throw new Error(err);
+        res.end();
+    });
+});
+
+module.exports = router;
+ */
